1a0902098cb87cbdd8dba5ae32bc5aa0f98a72bd
Atma1 <atmasusanto08@gmail.com>
2024-12-06 11:49:15 +0800

    feat: added routing html  file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: "/html/:slug",
+                destination: "/html/:slug.html",
+            }
+        ]
+    }
+};
 
 export default nextConfig;
