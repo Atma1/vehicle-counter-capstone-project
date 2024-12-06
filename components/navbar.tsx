5e15e23ba4c3343d248d06abde1648631e00bec7
Atma1 <atmasusanto08@gmail.com>
2024-12-06 11:50:43 +0800

    feat: added route to detect page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -15,8 +15,11 @@ export default function Navbar() {
         <Link href="/about" className="hover:text-primary">
           About Us
         </Link>
-        <Link 
-          href="/dashboard" 
+        <Link href="/html/detect" className="hover:text-primary">
+          Detect
+        </Link>
+        <Link
+          href="/dashboard"
           className="px-4 py-2 text-white bg-[#362222] rounded-md hover:bg-[#362222]/90"
         >
           Dashboard
