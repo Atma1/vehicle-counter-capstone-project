089f87843b2168ed6f165fec43701167af2f450a
Atma1 <atmasusanto08@gmail.com>
2024-12-07 17:36:44 +0800

    feat: added updateGraphData and triggerLine function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,47 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { GraphData, VehicleStats } from "@/app/dashboard/page"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function xywhToxyxy(xywh: number[]): number[] {
+  const x1 = xywh[0] - xywh[2] / 2;
+  const y1 = xywh[1] - xywh[3] / 2;
+  const x2 = xywh[0] + xywh[2] / 2;
+  const y2 = xywh[1] + xywh[3] / 2;
+  return [x1, y1, x2, y2];
+}
+
+export function triggerLine(xywh: number[], linePoint1: number[], linePoint2: number[], offset: number) {
+  const [, y1, , y2] = xywhToxyxy(xywh);
+
+  const bboxYCenter = Math.floor((y1 + y2) / 2)
+  const lineYCenter = Math.floor((linePoint1[1] + linePoint2[1]) / 2)
+  return lineYCenter < (bboxYCenter + offset) && lineYCenter > (bboxYCenter - offset);
+}
+
+export function updateCounter(cls: string, counter: VehicleStats) {
+  if (cls in counter) {
+    counter[cls as keyof VehicleStats]++;
+  }
+}
+
+export function updateGraphData(counter: VehicleStats, graphUpdate: GraphData) {
+  const timestamp = new Date().toLocaleTimeString();
+  graphUpdate.timestamps.push(timestamp);
+  graphUpdate.car.push(counter.car);
+  graphUpdate.truck.push(counter.truck);
+  graphUpdate.motorbike.push(counter.motorbike);
+  graphUpdate.bus.push(counter.bus);
+
+  if (graphUpdate.timestamps.length > 24) {
+    for (const key in graphUpdate) {
+      (graphUpdate[key as keyof GraphData] as number[]).shift();
+    }
+  }
+
+  return updateGraphData;
+}
+
