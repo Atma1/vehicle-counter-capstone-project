3c2665dd33ce8516cff9c1c66381d5b83fa18292
Atma1 <atmasusanto08@gmail.com>
2024-12-10 14:09:06 +0800

    feat: return data.data

--- action/getAllVehiceStats.ts
+++ action/getAllVehiceStats.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const getAllVehicleStats = async () => {
     try {
         const response = await axios.get(`${process.env.BACK_END_URL}/vehicle-stats`);
-        return response.data;
+        return response.data.data;
     } catch (error) {
         console.error('Error fetching vehicle stats:', error);
         throw error;
