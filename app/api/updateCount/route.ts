2fe568eb58a6634953a67b919c816223ee98bcdf
Atma1 <atmasusanto08@gmail.com>
2024-12-11 15:09:40 +0800

    fix: sql null bind query error and no response returned error

--- app/api/updateCount/route.ts
+++ app/api/updateCount/route.ts
@@ -1,30 +1,28 @@
-import { NextApiRequest } from 'next';
 import pool from '@/lib/db';
-import { NextResponse } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
 
-export default async function handler(req: NextApiRequest) {
+export async function POST(req: NextRequest) {
     if (req.method === 'POST') {
-        const { timestamp, location, car_count, motorbike_count, truck_count, bus_count } = req.body;
+        const body = await req.json();
+
+        const timestamp = new Date();
+
+        const { location, car_count, motorbike_count, truck_count, bus_count } = body;
 
         try {
             const query = `
                 INSERT INTO counts (timestamp, location, car_count, motorbike_count, truck_count, bus_count)
-                VALUES (?, ?, ?, ?, ?, ?)
-                ON DUPLICATE KEY UPDATE
-                    car_count = VALUES(car_count),
-                    motorbike_count = VALUES(motorbike_count),
-                    truck_count = VALUES(truck_count),
-                    bus_count = VALUES(bus_count)
-            `;
+                VALUES (?, ?, ?, ?, ?, ?);`;
             const values = [timestamp, location, car_count, motorbike_count, truck_count, bus_count];
-            const [result] = await pool.execute(query, values);
 
-            NextResponse.json({ message: 'Counts updated successfully', result }, { status: 201 });
+            await pool.execute(query, values);
+
+            return NextResponse.json({ message: 'Counts updated successfully', body }, { status: 201 });
         } catch (error) {
             console.error('Error updating counts:', error);
-            NextResponse.json({ error: 'Internal server error' }, { status: 500 });
+            return NextResponse.json({ error: 'Internal server error', message: error }, { status: 500 });
         }
     } else {
-        NextResponse.json({ message: 'Method not allowed' }, { status: 403 });
+        return NextResponse.json({ message: 'Method not allowed' }, { status: 403 });
     }
 }
