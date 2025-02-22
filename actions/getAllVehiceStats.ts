340bf80a4f62b49750340848cc09ccbbee7e17bf
Atma1 <atmasusanto08@gmail.com>
2025-02-22 17:30:24 +0800

    feat: move interface here and changed return value

--- actions/getAllVehiceStats.ts
+++ actions/getAllVehiceStats.ts
@@ -1,12 +1,22 @@
 import axios from 'axios';
+export interface VehicleStatsResponse {
+    count_id: number;
+    timestamp: string;
+    location: string;
+    car_count: number;
+    motorbike_count: number;
+    truck_count: number;
+    bus_count: number;
+}
 
-const getAllVehicleStats = async () => {
+
+const getAllVehicleStats = async (): Promise<VehicleStatsResponse[]> => {
     try {
         const response = await axios.get(`${process.env.BACK_END_URL}/api/vehicle-stats`);
         return response.data.data;
     } catch (error) {
-        console.error('Error fetching vehicle stats:', error);
-        throw error;
+        console.error(error);
+        return [];
     }
 };
 
