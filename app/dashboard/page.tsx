f1683baafa4977e58ef730e42e94e7f98bd6811e
Atma1 <atmasusanto08@gmail.com>
2025-02-25 15:28:20 +0800

    feat: moved get request to vehiclestatstable

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,5 @@
-import getAllVehicleStats from "@/actions/getAllVehiceStats";
 import VehicelStatsTable from "@/components/VehicelStatsTable";
 import { auth } from "@/app/auth";
-import { VehicleStatsResponse } from "@/actions/getAllVehiceStats";
 import { login } from "@/actions/auth";
 export const dynamic = "force-dynamic";
 
@@ -11,18 +9,12 @@ export default async function History() {
 
     if (!session) await login();
 
-    const res: VehicleStatsResponse[] = await getAllVehicleStats();
-
-    if (res.length == 0) {
-        return <h1 className="py-24 px-4 text-4xl font-bold text-center mb-12">No data</h1>
-    } else {
-        return (
-            <div className="py-24 px-4">
-                <div className="max-w-6xl mx-auto">
-                    <h1 className="text-4xl font-bold text-center mb-12">History</h1>
-                    <VehicelStatsTable data={res} />
-                </div>
+    return (
+        <div className="py-24 px-4">
+            <div className="max-w-6xl mx-auto">
+                <h1 className="text-4xl font-bold text-center mb-12">History</h1>
+                <VehicelStatsTable />
             </div>
-        )
-    }
+        </div>
+    )
 }
