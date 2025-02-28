c3b6fd9dea3a4cc42c98336df2432b7b85085b84
Atma1 <atmasusanto08@gmail.com>
2025-02-28 14:42:51 +0800

    feat: update navbar to use nextjs13-progress Link

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import Link from 'next/link'
+import { Link } from 'nextjs13-progress';
 import { Globe } from 'lucide-react'
 import { usePathname } from 'next/navigation';
 import MobileNavbar from './MobileNavbar';
