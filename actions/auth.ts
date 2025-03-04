4506c166893e7b9cb29d11c751b04a852ba2e555
Atma1 <atmasusanto08@gmail.com>
2025-03-04 14:42:40 +0800

    feat:  login function now accept providerId parameter

--- actions/auth.ts
+++ actions/auth.ts
@@ -2,6 +2,6 @@
 
 import { signIn, signOut } from '@/app/auth';
 
-export const login = async () => signIn('', { redirectTo: '/dashboard' });;
+export const login = async (providerId: string) => signIn(providerId, { redirectTo: '/dashboard' });
 
 export const logout = async () => await signOut({ redirectTo: '/' });
