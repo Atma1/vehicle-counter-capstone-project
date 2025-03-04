94849bd002bb3f132c37726ed33fcbecae24d312
Atma1 <atmasusanto08@gmail.com>
2025-03-04 14:43:36 +0800

    feat: refactor authentication providers to use providerMap for dynamic handling

--- app/auth.ts
+++ app/auth.ts
@@ -1,8 +1,21 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import type { Provider } from "next-auth/providers";
+
+const providers: Provider[] = [
+    GitHub
+]
+
+export const providerMap = providers.map((provider) => {
+    if (typeof provider === "function") {
+        const providerData = provider();
+        return { id: providerData.id, name: providerData.name }
+    } else {
+        return { id: provider.id, name: provider.name }
+    }
+})
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    theme: { logo: "https://authjs.dev/img/logo-sm.png" },
-    providers: [
-        GitHub]
+    providers,
+    pages: { signIn: '/login' },
 });
