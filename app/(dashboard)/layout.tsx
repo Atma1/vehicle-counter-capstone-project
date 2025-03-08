bf056dc83cfe2a9bc991374644ba0b1e7621803d
Atma1 <atmasusanto08@gmail.com>
2025-03-08 15:58:19 +0800

    refactor: remove authentication session check from DashboardLayout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,21 +1,14 @@
 import Sidebar from "@/components/sidebar";
-import { auth } from "@/app/auth";
-import { redirect } from "next/navigation";
 
 export default async function DashboardLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
-    const session = await auth();
-
-    // Redirect to login if no session exists
-    if (!session) return redirect('/login');
-
     return (
         <div>
             <Sidebar />
-            <div className="ml-80 mr-4 pt-12">
+            <div className="ml-80 mr-6 pt-12">
                 {children}
             </div>
         </div>
