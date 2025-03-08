290ebe85b711457257f26c83709e3d44efc3c184
Atma1 <atmasusanto08@gmail.com>
2025-03-08 15:55:07 +0800

    feat: updated import

--- actions/auth.ts
+++ actions/auth.ts
@@ -1,6 +1,6 @@
 "use server"
 
-import { signIn, signOut } from '@/app/auth';
+import { signIn, signOut } from '@/auth';
 
 export const login = async (providerId: string) => signIn(providerId, { redirectTo: '/dashboard' });
 
