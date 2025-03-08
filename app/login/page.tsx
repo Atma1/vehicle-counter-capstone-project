8787488e8aec1351987d6481fb379bf8e3fd4c61
Atma1 <atmasusanto08@gmail.com>
2025-03-08 15:58:04 +0800

    refactor: rename Page function to Login and update import path for auth

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,15 @@
 import { LoginForm } from "@/components/login-form"
-import { redirect } from "next/navigation"
-import { auth } from "@/app/auth";
+import { redirect } from "next/navigation";
+import { auth } from "@/auth";
 
-export default async function Page() {
+export default async function Login() {
   const session = await auth();
 
-  // Redirect to login if no session exists
+  // Redirect to dashboard if  session exists
   if (session) redirect('/dashboard');
+
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 bg-muted">
       <div className="w-full max-w-sm">
         <LoginForm />
       </div>
