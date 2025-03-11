c5dba8a9dd56ecd4109e7e130b542ca39bc44735
Atma1 <atmasusanto08@gmail.com>
2025-03-11 11:02:31 +0800

    feat: changed Sidebar menu and added icons

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,26 +3,27 @@
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { logout } from "@/actions/auth";
+import { Home, ChartArea } from 'lucide-react';
 
 export default function Sidebar() {
     const pathname = usePathname();
     return (
         <div
-            className='flex items-center gap-8 bg-white flex-col fixed top-[80px] left-0 w-[300px] bottom-0 p-8 border-r'>
-            <Link href="/"
-                className={pathname == "/" ? 'font-extrabold' : `hover:text-black text-gray-500`}>
-                Home
-            </Link>
-            <Link href="/about"
-                className={pathname == "/about" ? 'font-extrabold' : `hover:text-black text-gray-500`}>
-                About Us
-            </Link>
-            <Link href="/detections"
-                className={pathname == "/detections" ? 'font-extrabold' : `hover:text-black text-gray-500`}>
-                Detect
-            </Link>
+            className='flex items-center justify-between gap-8 bg-white flex-col fixed top-[80px] left-0 w-[300px] bottom-0 p-8 border-r'>
+            <div className="flex flex-col space-y-6">
+                <Link href="/dashboard"
+                    className={pathname == "/dashboard" ? 'font-extrabold' : `hover:text-black text-gray-500`}>
+                    <Home />
+                    Home
+                </Link>
+                <Link href="/dashboard"
+                    className={pathname == "/dashboard/analytics" ? 'font-extrabold' : `hover:text-black text-gray-500`}>
+                    <ChartArea />
+                    Analytics
+                </Link>
+            </div>
             <button
-                className="px-4 py-2 text-white bg-[#362222] rounded-md hover:bg-[#362222]/90"
+                className="px-10 py-3 text-white bg-[#362222] rounded-lg hover:bg-[#362222]/90"
                 onClick={() => logout()}>
                 Logout
             </button>
